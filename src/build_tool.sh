a612c546a20d317ffd68fd21063762cd916189b4
Tim Mayberry <mojofunk@gmail.com>
2018-02-10 15:28:47 +1000

    Move autotools related code into build_tool/autotools.sh module

--- src/build_tool.sh
+++ src/build_tool.sh
@@ -17,38 +17,3 @@ function set_build_tool
 
 	echo "Using build tool ${PKG_BUILD_TOOL}"
 }
-
-function autotools_set_env
-{
-	if [ -n "$HOST_SYSTEM" ]; then
-		HOST_SYSTEM_ARG=--host=${HOST_SYSTEM}
-	fi
-
-	# if BUILD_SYSTEM then BUILD_SYSTEM_ARG="--build=${BUILD_SYSTEM}"
-
-	# CFLAGS CPPFLAGS CXXFLAGS LDFLAGS ?
-
-	if [ -n "$PREFIX" ]; then
-		PREFIX_ARG=--prefix=${PREFIX}
-	fi
-
-	if [ -n "$PKG_VERBOSE_ENABLE" ]; then
-		CONFIGURE_VERBOSE_ARG="--verbose"
-		MAKE_VERBOSE_ARG="V=1"
-	fi
-}
-
-function autotools_configure
-{
-	./configure $CONFIGURE_VERBOSE_ARG $PREFIX_ARG $HOST_SYSTEM_ARG ${1}
-}
-
-function autotools_build
-{
-	make $MAKE_VERBOSE_ARG $SMP_MAKEFLAGS
-}
-
-function autotools_install
-{
-	make $MAKE_VERBOSE_ARG install
-}
--- src/build_tool/autotools.sh
+++ src/build_tool/autotools.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
 
+function autotools_set_env
+{
+	if [ -n "$HOST_SYSTEM" ]; then
+		HOST_SYSTEM_ARG=--host=${HOST_SYSTEM}
+	fi
+
+	# if BUILD_SYSTEM then BUILD_SYSTEM_ARG="--build=${BUILD_SYSTEM}"
+
+	# CFLAGS CPPFLAGS CXXFLAGS LDFLAGS ?
+
+	if [ -n "$PREFIX" ]; then
+		PREFIX_ARG=--prefix=${PREFIX}
+	fi
+
+	if [ -n "$PKG_VERBOSE_ENABLE" ]; then
+		CONFIGURE_VERBOSE_ARG="--verbose"
+		MAKE_VERBOSE_ARG="V=1"
+	fi
+}
+
+function autotools_configure
+{
+	./configure $CONFIGURE_VERBOSE_ARG $PREFIX_ARG $HOST_SYSTEM_ARG ${1}
+}
+
+function autotools_build
+{
+	make $MAKE_VERBOSE_ARG $SMP_MAKEFLAGS
+}
+
+function autotools_install
+{
+	make $MAKE_VERBOSE_ARG install
+}
+
 function build_tool_set_env
 {
 	autotools_set_env
