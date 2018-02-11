3912c7f2f9dc212eac20747f7254727046f3b3ad
Tim Mayberry <mojofunk@gmail.com>
2018-02-11 10:52:49 +1000

    Print download URL when fetching sources

--- src/utils.sh
+++ src/utils.sh
@@ -8,6 +8,7 @@ function download
 function download_source_and_unpack
 {
 	cd $PKG_SRC_ROOT_DIR
+	echo "Downloading $PKG_SRC_URL ..."
 	download $PKG_SRC_FILE $PKG_SRC_URL || exit 1
 	tar xf ${PKG_SRC_FILE}
 	# must not exist
