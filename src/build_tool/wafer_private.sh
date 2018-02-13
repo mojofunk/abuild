3678f89124b3ab077ee6df1861737f76493230ee
Tim Mayberry <mojofunk@gmail.com>
2018-02-13 21:38:32 +1000

    Use PYTHON env var to call wafer script rather than rely on shebang line

--- src/build_tool/wafer_private.sh
+++ src/build_tool/wafer_private.sh
@@ -2,22 +2,21 @@
 
 function wafer_set_env
 {
+	#waf_set_env ?
 	TOOLSET_ARG="--toolset=${TOOLSET}"
 }
 
 function wafer_configure
 {
-	#waf_set_env
-	#wafer_set_env
-	./wafer ${PKG_VERBOSE_OPTION} configure $TOOLSET_ARG $PREFIX_ARG $LIBDIR_ARG ${1}
+	$PYTHON wafer ${PKG_VERBOSE_OPTION} configure $TOOLSET_ARG $PREFIX_ARG $LIBDIR_ARG ${1}
 }
 
 function wafer_build
 {
-	./wafer ${PKG_VERBOSE_OPTION} build $SMP_MAKEFLAGS ${1}
+	$PYTHON wafer ${PKG_VERBOSE_OPTION} build $SMP_MAKEFLAGS ${1}
 }
 
 function wafer_install
 {
-	./wafer ${PKG_VERBOSE_OPTION} install ${1}
+	$PYTHON wafer ${PKG_VERBOSE_OPTION} install ${1}
 }
